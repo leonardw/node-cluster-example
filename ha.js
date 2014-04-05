90e1d6a198a972a104bc28048493284eb2bfbe41
Len <leonard.wu92@alumni.imperial.ac.uk>
2014-04-05 01:33:55 +0100

    Correctly remove non-respawn worker entry from tracking; Use printf style console log; Only show respawn message if worker is configured as such

--- ha.js
+++ ha.js
@@ -2,17 +2,17 @@ var cluster = require('cluster'),
 	path = require('path');
 
 if (!cluster.isMaster) {
-	console.warn('Terminated. Attempt to run Master in Worker mode.');
+	console.error('Terminated. Attempt to run Master in Worker mode.');
 	process.exit(1);
 }
 
 var CPU_CORES = require('os').cpus().length;
 
-var workers = require('./config').workers;
-var respawnWorkers = {};
+var workerConfig = require('./config').worker;
+var workerEnv = {};
 
 function showWorkers() {
-	console.log('workers:', respawnWorkers);
+	console.log('workers:', workerEnv);
 //	for (var id in cluster.workers) {
 //		console.log('worker '+id, cluster.workers[id]);
 //	}
@@ -20,7 +20,7 @@ function showWorkers() {
 
 
 var extargs = process.argv.slice(2);
-// purely cosmetic for now; will replace fork(ENV) once issue is fixed
+// purely cosmetic for now; this replaces fork(ENV) once issue is fixed
 // https://github.com/joyent/node/issues/4149
 extargs.push('--slave');
 
@@ -32,9 +32,9 @@ cluster.setupMaster({
 	args : extargs
 });
 
-// Fork worker processes
-for (var role in workers) {
-	var prop = workers[role];
+// fork worker processes
+for (var role in workerConfig) {
+	var prop = workerConfig[role];
 	var instance = (prop.instance === 'cpu')? CPU_CORES : prop.instance;
 	for (var i = 0; i < instance; i++) {
 		var env = {
@@ -43,33 +43,32 @@ for (var role in workers) {
 			NODE_WORKER_ROLE_RESPAWN: prop.respawn
 		};
 		var worker = cluster.fork(env);
-		respawnWorkers[worker.id] = env;
+		workerEnv[worker.id] = env;
 	}
 }
 
-function workerInfo(worker) {
-	return 'Worker ' + worker.id + ' ' + respawnWorkers[worker.id]['NODE_WORKER_ROLE'] + ' (PID ' + worker.process.pid + ')';
-}
-
 cluster.on('online', function(worker) {
-	console.info(workerInfo(worker) + ' online');
+	console.info('Worker %d %s (PID %d) online', worker.id, workerEnv[worker.id].NODE_WORKER_ROLE, worker.process.pid);
 });
 
 cluster.on('listening', function(worker, address) {
-	console.info(workerInfo(worker) + ' listening on ' + address.address + ':' + address.port);
+	console.info('Worker %d %s (PID %d) listening on %s:%d', worker.id, workerEnv[worker.id].NODE_WORKER_ROLE, worker.process.pid, address.address, address.port);
 });
 
 cluster.on('disconnect', function(worker) {
-	console.info(workerInfo(worker) + ' disconnected');
+	console.warn('Worker %d %s (PID %d) disconnected', worker.id, workerEnv[worker.id].NODE_WORKER_ROLE, worker.process.pid);
 });
 
 cluster.on('exit', function(worker, code, signal) {
-	console.warn(workerInfo(worker) + (code?' exit with code '+code : (signal?' killed due to '+signal:' exit')));
-	var env = respawnWorkers[worker.id];
-	if (env && env.NODE_WORKER_ROLE_RESPAWN) {
-		delete respawnWorkers[worker.id];
+	var env = workerEnv[worker.id],
+		respawn = env && env.NODE_WORKER_ROLE_RESPAWN;
+	
+	console.warn('Worker %d %s (PID %d) died (%s).%s', worker.id, env.NODE_WORKER_ROLE, worker.process.pid, signal||code, respawn?' Respawning...':'');
+	
+	delete workerEnv[worker.id];
+	if (respawn) {
 		var spawn = cluster.fork(env);
-		respawnWorkers[spawn.id] = env;
+		workerEnv[spawn.id] = env;
 	}
 	//showWorkers();
 });
