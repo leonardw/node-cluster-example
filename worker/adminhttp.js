83dbb9bed7a7efc6e27ab0b03b63677e494d685d
Len <leonard.wu92@alumni.imperial.ac.uk>
2014-04-06 07:59:18 +0100

    Fix incorrect adminhttp port

--- worker/adminhttp.js
+++ worker/adminhttp.js
@@ -4,4 +4,4 @@ var http = require('http');
 http.createServer(function(req, res) {
 	res.writeHead(200);
 	res.end('adminhttp PID ' + process.pid + '\n<pre>' + JSON.stringify(process.env) +'</pre>');
-}).listen(8000);
+}).listen(8001);
